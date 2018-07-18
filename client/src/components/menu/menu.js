2c9143a151cacab2d9e6b92e2caa7c3e5d975de3
DESKTOP-A8I01GK\FernoMazer <Kittipan_panyapusit@cmu.ac.th>
2018-07-18 21:55:08 +0700

    update icon

--- client/src/components/menu/menu.js
+++ client/src/components/menu/menu.js
@@ -5,7 +5,7 @@ class Menu extends Component {
   render() {
     return (
       <nav id="menubar" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a id="sig-logo" class="navbar-brand logo" href="/">Signature</a>
+        <img href="/" src="%PUBLIC_URL%/logo128.icog" />
         <button class="navbar-toggler" type="button" data-toggle="collapse"
           data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
           aria-expanded="false" aria-label="Toggle navigation">
