8f494b4a69fd4d635d4fd559c10de004213e95ee
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2018-07-19 14:26:14 +0700

    retreive data from firebase to product

--- client/src/components/dashboard/product-list/product-item.js
+++ client/src/components/dashboard/product-list/product-item.js
@@ -1,11 +1,15 @@
-'use strict';
-
 import React, { Component } from 'react';
-
+import './product-item.css'
 class ProductItem extends Component {
   render() {
+    const {product} = this.props
     return (
-      <div></div>
+      <div class="col-sm">
+      <img id="product-item" src={product.image} width="100" height="100"/>
+      <div><b>{product.name}</b></div>
+      <div>{product.description}</div>
+      <div>Price: {product.price}</div>
+      </div>
     );
   }
 }
--- client/src/components/dashboard/product-list/product-list.js
+++ client/src/components/dashboard/product-list/product-list.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React, { Component } from 'react';
 import ProductItem from './product-item';
 import FirebaseService from '../../../services/firebase'
@@ -7,15 +5,23 @@ import FirebaseService from '../../../services/firebase'
 class ProductList extends Component {
   constructor(props) {
     super(props);
-    this.state = { products: [] }; // <- set up react state
+    this.state = { products: [] };
   }
-  componentWillMount(){
-    
+  componentDidMount(){
+    FirebaseService.child('products').on('value', snap =>{
+      this.setState({products: snap.val()});
+    })
   }
   render() {
     return (
-      <div>
-        Hello from Product list
+      <div class="container">
+        <div class="row">
+        {
+          this.state.products.map((product,key)=>
+            <ProductItem key = {key} product = {product}/>
+          )
+        }
+        </div>
       </div>
     );
   }
