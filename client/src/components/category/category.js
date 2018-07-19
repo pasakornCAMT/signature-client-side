14547000844a85258f7246f8d36afef9990f46d9
Tanee <tanee_yooyen@cmu.ac.th>
2018-07-19 12:36:25 +0700

    add category

--- client/src/components/category/category.js
+++ client/src/components/category/category.js
@@ -8,16 +8,22 @@ class Category extends Component {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="c nav-link">Category 1</a>
+              <a class="c nav-link" id="allproduct">All Product</a>
             </li>
             <li class="nav-item">
-              <a class="c nav-link">Category 2</a>
+              <a class="c nav-link" id="popular">Popular</a>
             </li>
             <li class="nav-item">
-              <a class="c nav-link">Category 3</a>
-            </li>
-            <li class="nav-item">
-              <a class="c nav-link">Category 4</a>
+              <div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Category
+  </a>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Food</a>
+    <a class="dropdown-item" href="#">Gift</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
             </li>
           </ul>
         </div>
--- client/src/components/dashboard/home.js
+++ client/src/components/dashboard/home.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './home.css';
 import FirstImg from './sss-web.png';
-// import Category from './components/category/category';
+import Category from '../category/category';
 
 class Home extends Component {
   render() {
     return (
       <div class="gatsby-highlight">
         <img id="sss-web" src={FirstImg} alt="web img"/>
+              <Category/>
       </div>
+
     );
   }
 }
