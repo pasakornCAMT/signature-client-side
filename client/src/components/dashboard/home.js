ea0eeeb1a8de845b0e45a994abc6066f0d39bc22
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2018-07-19 12:38:34 +0700

    install firebase and create product list component

--- client/src/components/dashboard/home.js
+++ client/src/components/dashboard/home.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import './home.css';
 import FirstImg from './sss-web.png';
 // import Category from './components/category/category';
+import ProductList from './product-list/product-list'
 
 class Home extends Component {
   render() {
     return (
+      <div>
       <div class="gatsby-highlight">
         <img id="sss-web" src={FirstImg} alt="web img"/>
+      </div>
+      <div>
+        <ProductList/>
+      </div>
       </div>
     );
   }
---
+++ client/src/components/dashboard/product-list/product-list.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+import ProductItem from './product-item';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        Hello from Product list
+      </div>
+    );
+  }
+}
+
+export default ProductList;
