0f85ad4d8a70b2a517d947ea13b8ed0dcfc223a4
Tanee <tanee_yooyen@cmu.ac.th>
2018-07-20 12:57:00 +0700

    add card

--- client/src/components/dashboard/product-list/product-item.js
+++ client/src/components/dashboard/product-list/product-item.js
@@ -4,12 +4,18 @@ class ProductItem extends Component {
   render() {
     const {product} = this.props
     return (
-      <div class="col-sm-4">
-      <img id="product-item" src={product.image} width="100" height="100"/>
-      <div><b>{product.name}</b></div>
-      <div>{product.description}</div>
-      <div>Price: {product.price} THB</div>
+
+
+<div class="col-sm-4">
+      <div class="card" style={{width: '18rem'}}>
+        <img id="product-item" src={product.image} width="100" height="100"/>
+        <div class="card-body">
+          <div><b>{product.name}</b></div>
+          <div>{product.description}</div>
+          <div>Price: {product.price} THB</div>
+          </div>
       </div>
+</div>
     );
   }
 }
