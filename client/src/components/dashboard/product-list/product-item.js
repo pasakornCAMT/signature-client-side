5c114befcf0a69f3c561e3f0ee6bd1a7946636d4
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2018-07-20 12:50:20 +0700

    extend image size

--- client/src/components/dashboard/product-list/product-item.js
+++ client/src/components/dashboard/product-list/product-item.js
@@ -4,11 +4,11 @@ class ProductItem extends Component {
   render() {
     const {product} = this.props
     return (
-      <div class="col-sm">
-      <img id="product-item" src={product.image} width="100" height="100"/>
+      <div class="col-sm-4">
+      <img id="product-item" src={product.image} width="300" height="300"/>
       <div><b>{product.name}</b></div>
       <div>{product.description}</div>
-      <div>Price: {product.price} THB</div>
+      <div><b>Price: {product.price} THB</b></div>
       </div>
     );
   }
